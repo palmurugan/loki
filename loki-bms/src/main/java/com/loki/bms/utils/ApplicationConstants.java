4a12b877db64baf28af9b293e726ba36a71461e1
Palmurugan Chandran <palmuruganc@suntecsbs.com>
2020-02-24 11:44:10 +0530

    Application Constants Added

--- loki-bms/src/main/java/com/loki/bms/utils/ApplicationConstants.java
+++ loki-bms/src/main/java/com/loki/bms/utils/ApplicationConstants.java
@@ -6,7 +6,7 @@ public class ApplicationConstants {
     public static final String BMS_PACKAGE = "com.loki.bms";
     public static final String CONFIGURATION_PACKAGE = "com.loki.configuration";
     public static final String CUSTOMER_PACKAGE = "com.loki.customer";
-    public static final String ITEM_PACKAGE = "com.loki.customer";
+    public static final String ITEM_PACKAGE = "com.loki.item";
 
     public static final String CONFIG_ENTITY_PKG = "com.loki.configuration.core.entity";
     public static final String CUSTOMER_ENTITY_PKG = "com.loki.customer.core.entity";
